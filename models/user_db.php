66caedf09383297f42fc6ae310316c25a1d658b1
sh615576 <sh615576@LR00207.southeast.edu>
2019-11-11 13:47:01 -0600

    Merge origin/master
    Conflicts:
    	models/appointment_db.php
    	models/user_db.php

--- models/user_db.php
+++ models/user_db.php
@@ -170,7 +170,7 @@ class user_db
          VALUES
          (:fName, :lName, :email, :phone, :role, :password)';
 
-        $role = Student;
+        $role = 'Student';
         $statement = $db->prepare($query);
         //bind the values
         $statement->bindValue(':fName', $firstName);
