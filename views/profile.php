c153521ecfe07c2ec9250c1245e865cc1fc8251a
shookstra <samhookstra@live.com>
2019-12-11 19:29:32 -0600

    Added styling to update user info

--- views/profile.php
+++ views/profile.php
@@ -20,8 +20,8 @@
             <p><?php echo htmlspecialchars('Email: ' . $_SESSION['user']->getEmail()); ?></p>
             <p><?php echo htmlspecialchars('Phone Number: ' . $phoneNumber); ?></p>
             <form>
-                <input type="submit" value="Change My Information">
-                  <input type="hidden" value="ChangeMyInformation" name="action">
+                <input type="submit" value="Change My Information" class="update-info-button">
+                <input type="hidden" value="ChangeMyInformation" name="action">
             </form>
         </div>
     </div>
--- views/updateProfile.php
+++ views/updateProfile.php
@@ -1,51 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/GroupProject/views/head.php') ?>
 
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/GroupProject/views/header.php');
+    include('views/sideBar.php');
+    ?>
 
-    <?php include($_SERVER['DOCUMENT_ROOT'] . '/GroupProject/views/head.php') ?>
-
-    <body>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/GroupProject/views/header.php');
-        include('views/sideBar.php');
-        ?>
-
-        <div class="wrapper">
-            <div style="flex: 4">
-                <h3 style="font-weight: bold">Profile Information</h3>
-                  <form method="POST">
-                <p><label>User ID: </label><input type="text" name="userID" value="<?php echo htmlspecialchars($_SESSION['user']->getUserID()); ?>" readonly></p>
-                <p><label>First Name: </label><input type="text" name="firstName" value="<?php if (!empty($firstName)) {
-            echo $firstName;
-        } ?>"></p>
-                <p> <label>Last Name: </label><input type="text" name="lastName" value="<?php if (!empty($lastName)) {
-            echo $lastName;
-        } ?>"></p>
-                <p><label>Phone Number: </label><input type="text" name="phone" value="<?php if (!empty($phone)) {
-                        echo $phone;
-                    } ?>"></p>
-
-                    <?php if (empty($error)) { ?>
-                    <div>
+    <div class="wrapper">
+        <form method="POST" class="update-info-form">
+            <h3 class="update-info-form-title">Update Your Information</h3>
+            <div class="update-info-form-content">
+                <label>User ID </label><input type="text" class="static-info-input" name=" userID" value="<?php echo htmlspecialchars($_SESSION['user']->getUserID()); ?>" readonly></<label>
+                <label>First Name </label><input type="text" class="update-info-input" name="firstName" value="<?php if (!empty($firstName)) {
+                                                                                                                    echo htmlspecialchars($firstName);
+                                                                                                                } ?>">
+                <label>Last Name </label><input type="text" class="update-info-input" name="lastName" value="<?php if (!empty($lastName)) {
+                                                                                                                    echo htmlspecialchars($lastName);
+                                                                                                                } ?>"></<label>
+                <label>Phone Number </label><input type="text" class="update-info-input" name="phone" value="<?php if (!empty($phone)) {
+                                                                                                                    echo htmlspecialchars($phone);
+                                                                                                                } ?>"></<label>
+                <?php if (empty($error)) { ?>
                     <?php
-                    if (!empty($updateErrors)) {
-                        echo '<div class="error">';
-                        foreach ($updateErrors as $error) {
-                            echo '<p>' . htmlspecialchars($error) . '</p>';
-                        }
-                        echo '</div>';
-                    }
-                    ?>
-                    </div>
-<?php } ?>
-              
-                    <input type="hidden" name="action" id="action" value="updateValidation">
-                    <input type="submit" value="Update Information" name="submit">
-                </form>
+                        if (!empty($updateErrors)) { ?>
+                        <div class="error">
+                            <?php foreach ($updateErrors as $error) { ?>
+                                <p><?php echo htmlspecialchars($error); ?></p>
+                            <?php } ?>
+                        </div>
+                    <?php } ?>
+                <?php } ?>
+
+                <input type="hidden" name="action" id="action" value="updateValidation">
+                <input type="submit" value="Update Information" name="submit" class="update-info-button">
             </div>
-        </div>
-
-
-        <script src="scripts/profileModal.js"></script>
-    </body>
+        </form>
+    </div>
+    <script src="scripts/profileModal.js"></script>
+</body>
 
 </html>
